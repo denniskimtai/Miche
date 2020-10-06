2cf4fd6610267c8efa25faba10c6fe8d917a29b2
denniskimtai <denniskimtai1@gmail.com>
2020-10-06 16:03:37 +0300

    service provider dashboard

--- app/src/main/java/com/example/myapplication/ServiceProviderHome.java
+++ app/src/main/java/com/example/myapplication/ServiceProviderHome.java
@@ -1,14 +1,14 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class FarmersSignUpActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ServiceProviderHome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_farmers_sign_up);
+        setContentView(R.layout.activity_service_provider_home);
     }
 }
